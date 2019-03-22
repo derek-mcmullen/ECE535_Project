1624d078913225f544d9ec22bb3fe7a50f168db5
jakereed69 <jakereed69>
2019-03-22 14:12:29 -0700

    added comments

--- plot_tools.cpp
+++ plot_tools.cpp
@@ -1,18 +1,29 @@
-#include "plot_tools.h"
+/* 
+Author: Jake Reed
+Date: 3/21/19
 
-/* Simple demo of gnuplot
+Simple demo of gnuplot
    essentially use system to call a batch file that calls
    local version of gnuplot and passes in a 'gnuplot file' which has
    all the gnuplot commands to plot. 
 
    All plotting configuration should take place within the plot.txt file.
 */
+
+#include "plot_tools.h"
+
 int demo(float pi){
+
+	// Create some iterators
 	float x = 0;
 	float i = 0;
+
+	// Open file for storing 'signal'
 	cout << "Opening file...\n" << endl;
 	ofstream signalFile;
 	signalFile.open("signal.txt");
+
+	// Create a sine wave with .1 units between sample, for 3 cycles
 	for (int z = 0; z < 3; z++){
 		while (i < 2 * pi){
 			x = sin(i);
@@ -25,6 +36,8 @@ int demo(float pi){
 	}
 	signalFile.close();
 	cout << "Closing File...";
+
+	// Call batch file that handles gnuplot
 	cout << "Plotting...";
 	system("plotter.bat");
 	system("pause");
--- plot_tools.h
+++ plot_tools.h
@@ -1,3 +1,17 @@
+/*
+Author: Jake Reed
+Date: 3/21/19
+
+Simple demo of gnuplot
+essentially use system to call a batch file that calls
+local version of gnuplot and passes in a 'gnuplot file' which has
+all the gnuplot commands to plot.
+
+All plotting configuration should take place within the plot.txt file.
+*/
+
+
+
 #ifndef plot_tools_h
 #define plot_tools_h
 
