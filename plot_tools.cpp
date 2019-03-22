088d250993d118df4b95034687059dc2c4de29fa
jakereed69 <jakereed69>
2019-03-22 14:42:20 -0700

    summary of 2018

--- plot_tools.cpp
+++ plot_tools.cpp
@@ -11,35 +11,47 @@ Simple demo of gnuplot
 */
 
 #include "plot_tools.h"
+#include <random>
+
+#define DEBUG false 
 
 int demo(float pi){
 
 	// Create some iterators
 	float x = 0;
 	float i = 0;
+	float amplitude = 0;
+	float noise;
 
+	cout << "Please Enter Noise Amplitude Between 0-1 ...\n";
+	cin >> amplitude;
+	cout << "\n\nThats what i thought bitch...\n";
+	cout << "Allow me to show you how gay you were in 2018...\n\n\n";
 	// Open file for storing 'signal'
-	cout << "Opening file...\n" << endl;
+	cout << "Opening file..." << endl;
 	ofstream signalFile;
 	signalFile.open("signal.txt");
 
 	// Create a sine wave with .1 units between sample, for 3 cycles
-	for (int z = 0; z < 3; z++){
+	for (int z = 0; z < 30; z++){
 		while (i < 2 * pi){
-			x = sin(i);
+			noise = ((double)rand() / (RAND_MAX)) + 1;
+			x = sin(i) + (amplitude*noise);
 			signalFile << x << "\n";
-			cout << x << "\n";
 			i = i + .1;
-			cout << "i=" << i << "\n";
+			if (DEBUG){
+				cout << x << "\n";
+				cout << "i=" << i << "\n";
+			}
 		}
 		i = x = 0;
 	}
 	signalFile.close();
-	cout << "Closing File...";
+	cout << "Closing File...\n";
 
 	// Call batch file that handles gnuplot
-	cout << "Plotting...";
+	cout << "Plotting...\n";
 	system("plotter.bat");
-	system("pause");
+	 system("pause");
 	return 0;
 }
