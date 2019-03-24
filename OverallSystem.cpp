d460d34bb87905a69bc6bab04347bcc26a57292b
dmcmullen <derek.m.mcmullen@gmail.com>
2019-03-24 12:58:47 -0700

    relocated the config lead function to its own source

--- OverallSystem.cpp
+++ OverallSystem.cpp
@@ -1,3 +1,4 @@
+#include "ConfigLoader.h"
 #include "InterferenceSystem.h"
 #include "Sampler.h"
 
@@ -13,22 +14,16 @@
 
 using namespace std; 
 
-// function prototypes
-void loadSettings(); 
-
-// variables that will be set from config file
-double carrier_freq, sample_rate, n_ary;
-std::string mod_type;
-
-
-
-
 int main()
 {
-	loadSettings(); 
+	// config data struct
+	inData configInData; 
+
+	loadSettings( configInData ); 
 
 	// testing that files have been parsed appropriately
-	cout << mod_type << endl; 
+	cout << configInData.mod_type << endl; 
+
 	//demo(PI);
 	system("pause"); 
 
@@ -36,48 +31,3 @@ int main()
 
 
 
-
-void loadSettings() {
-	std::ifstream cFile("settings/config.txt");
-
-	if (cFile.is_open())
-	{
-		std::string line;
-		while (getline(cFile, line)) {
-			line.erase(std::remove_if(line.begin(), line.end(), isspace),
-				line.end());
-			if (line[0] == '#' || line.empty())
-				continue;
-			auto delimiterPos = line.find("=");
-			auto name = line.substr(0, delimiterPos);
-			auto value = line.substr(delimiterPos + 1);
-			// std::cout << name << " " << value << '\n';
-
-			if (strcmp(name.c_str(), "mod_type") == 0)
-			{
-				mod_type = value;
-			}
-			else if (strcmp(name.c_str(), "carrier_freq") == 0)
-			{
-				carrier_freq = stod(value);    // string to double 
-			}
-			else if (strcmp(name.c_str(), "sample_rate") == 0)
-			{
-				sample_rate = stod(value);    // string to double 
-			}
-			else if (strcmp(name.c_str(), "n_ary") == 0)
-			{
-				n_ary = stoi(value);    // string to double 
-			}
-			/* more else if clauses */
-			else /* default: */
-			{
-				std::cout << "Value for " << name << " was not loaded properly" << std::endl;
-			}
-		}
-
-	}
-	else {
-		std::cerr << "Couldn't open config file for reading.\n";
-	}
-}
